361a11bff2b0fd2070a1d0abcb4c7038347197fb
Milosz <milosz.matejko@gmail.com>
2019-07-14 19:16:04 +0200

    added searching by Name

--- src/main/java/com/miloszmatejko/mysqlaccess/controller/MainController.java
+++ src/main/java/com/miloszmatejko/mysqlaccess/controller/MainController.java
@@ -1,11 +1,16 @@
 package com.miloszmatejko.mysqlaccess.controller;
 
+
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.miloszmatejko.mysqlaccess.entity.User;
 import com.miloszmatejko.mysqlaccess.repos.UserRepository;
 
@@ -37,4 +42,22 @@ public class MainController {
 		model.addAttribute("userList", userRepository.findAll());
 		return "userList";
 	}
+	
+	
+	@GetMapping("/findUser")
+	public String findUser(Model model) {
+		model.addAttribute("user", new User());
+		return "userSearch";
+	}
+	
+	
+	@PostMapping("/findByName")
+	public String findUser(@RequestParam String name, Model model) {
+		
+		model.addAttribute("userList", userRepository.findByName(name));
+		
+		return "userList";
+	}
+	
+	
 }
--- src/main/java/com/miloszmatejko/mysqlaccess/repos/UserRepository.java
+++ src/main/java/com/miloszmatejko/mysqlaccess/repos/UserRepository.java
@@ -1,9 +1,12 @@
 package com.miloszmatejko.mysqlaccess.repos;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.miloszmatejko.mysqlaccess.entity.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
+	List<User> findByName(String name);
 }
